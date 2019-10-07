6ef9eaf38f93f834f6ba0523f8158052e455762c
Jeff Jackson <Jeffrey.Jackson@dfo-mpo.gc.ca>
2019-10-07 13:27:38 -0300

    Initial working CTD profile plotting tool

--- VisualEditor/app.R
+++ VisualEditor/app.R
@@ -1,3 +1,4 @@
+
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -14,127 +15,147 @@ library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
-    tags$head(
-        tags$style(HTML('* {font-family: BentonSans Book; font-size: 16px}'))
-    ),
-    
-    # Style horizontal rules (hr).
-    tags$head(
-        tags$style(HTML('hr {border-top: 1px solid #000000}'))
-    ),
-    
-    # Create the title and style it.
-    h1(id="big-heading", "Visual QC Editor"),
-    tags$head(tags$style(
-        HTML('#big-heading {
+  
+  tags$head(
+    tags$style(HTML('* {font-family: BentonSans Book; font-size: 16px}'))
+  ),
+  
+  # Style horizontal rules (hr).
+  tags$head(
+    tags$style(HTML('hr {border-top: 1px solid #000000}'))
+  ),
+  
+  # Create the title and style it.
+  h1(id="big-heading", "Visual QC Editor"),
+  tags$head(tags$style(
+    HTML('#big-heading {
       color: red; 
       font-size: 40px;
       font-style: bold;
     }')
-    )),
-    
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-
-            br(),
-            
-            # Press button to load an ODF file.
-            p(style = "text-align: center; border-color: #f00", 
-              actionButton("loadButton",tags$b("Load ODF File"))),
-            tags$style("#loadButton{color: blue; font-size: 20px; background-color: yellow; border-color: black; border-width: 5px}"),
-
-            br(),
-
-            # Text box containing file path and name.
-            div
-            ( style = "font-size:20px;", 
-                textInput(inputId = "filepath", 
-                          label = div(style = "font-size:20px", "Selected ODF File:"), 
-                          value = ""
-                )
-            ),
-            
-            # Insert Horizontal Rule (line) ----
-            # hr(),
-
-            selectInput(inputId = "parameter", label = strong("Select X Parameter:"), choices = NULL)
-            
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            
-            # textOutput("filep"),
-            # textOutput("params"),
-            textOutput("cn"),
-            # textOutput("nms"),
-            
-            plotlyOutput("profilePlot")
-            
+  )),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      
+      br(),
+      
+      # Press button to load an ODF file.
+      p(style = "text-align: center; border-color: #f00", 
+        actionButton("loadButton",tags$b("Load ODF File"))),
+      tags$style("#loadButton{color: blue; font-size: 20px; background-color: yellow; border-color: black; border-width: 5px}"),
+      
+      br(),
+      
+      # Text box containing file path and name.
+      div
+      ( style = "font-size:20px;", 
+        textInput(inputId = "filepath", 
+                  label = div(style = "font-size:20px", "Selected ODF File:"), 
+                  value = ""
         )
+      ),
+      
+      # Insert Horizontal Rule (line) ----
+      # hr(),
+      
+      selectInput(inputId = "parameter", label = strong("Select X Parameter:"), choices = NULL)
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      # textOutput("filep"),
+      # textOutput("params"),
+      # textOutput("cn"),
+      # textOutput("param"),
+        
+      plotlyOutput("profilePlot")
+      
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
-    re1 <- eventReactive(
-        input$loadButton, 
-        {
-            fileWithPath <- file.choose()
-        }
-    )
+  
+  re1 <- eventReactive(
+    input$loadButton, 
+    {
+      fileWithPath <- file.choose()
+    }
+  )
+  
+  output$filep <- renderText({
+    odfFile <- basename(re1())
+  })
+  
+  observe({
     
-    output$filep <- renderText({
-        odfFile <- basename(re1())
-    })
-
-    observe({
-        
-        # Display the current ODF file with path.
-        odfFile <- basename(re1())
-
-        # Display the current ODF file without path in the textInput box.
-        updateTextInput(session, "filepath", value = as.character(odfFile))
-        
-    })
-
-    odfData <- reactiveValues()
+    # Display the current ODF file with path.
+    odfFile <- basename(re1())
     
-    observe({
-        # Read in the CTD data.
-        ctd <- read.ctd(re1())
-        odfData$dfCTD <- as.data.frame(ctd[['data']])
-        odfData$lMeta <- as.list(ctd[['metadata']])
-        # nms <- as.numeric(row.names(odfData$dfCTD))
-    })
-
-    output$cn <- renderText({
-        print(odfData$lMeta$cruiseNumber)
-    })
+    # Display the current ODF file without path in the textInput box.
+    updateTextInput(session, "filepath", value = as.character(odfFile))
     
-    vars <- reactive({
-        vars <- names(odfData$dfCTD)
-    })
-
-    output$nms <- reactive({
-        print(odfData$nms)
-    })
-    
-    observe({
-        updateSelectInput(session, "parameter", choices = vars(), selected = "temperature")
-    })
-    
-    # output$profilePlot <- renderPlotly({
-    #     if (is.null(odfData)) {return()}
-    #     p = ggplot()
-    #     p <- ggplot(data = odfData$dfCTD, aes(x = input$parameter, y = pressure)) + geom_point()
-    #     # p <- p + scale_y_reverse()
-    #     ggplotly(p) %>% layout(dragmode = "lasso")
-    # })
-    
+  })
+  
+  odfData <- reactiveValues()
+  
+  observe({
+    # Read in the CTD data.
+    ctd <- read.ctd(re1())
+    odfData$dfCTD <- as.data.frame(ctd[['data']])
+    odfData$lMeta <- as.list(ctd[['metadata']])
+    # nms <- as.numeric(row.names(odfData$dfCTD))
+  })
+  
+  output$cn <- renderText({
+    print(odfData$lMeta$cruiseNumber)
+  })
+  
+  vars <- reactive({
+    vars <- names(odfData$dfCTD)
+  })
+  
+  output$nms <- reactive({
+    print(odfData$nms)
+  })
+  
+  observe({
+    updateSelectInput(session, "parameter", choices = vars(), selected = "temperature")
+  })
+  
+  var <- reactive({
+      input$parameter
+  })
+  
+  output$param <- renderText({
+    var()   
+  })
+  
+  df <- reactive({
+      odfData$dfCTD
+  })
+  
+  output$profilePlot <- renderPlotly({
+      
+      # Make sure that the selectInput drop down list box contains something before continuing.
+      req(input$parameter)
+      
+      # If there is no CTD loaded yet then do nothing.
+      if (is.null(df())) {return()}
+      
+      # Produce the profile plot.
+      p = ggplot()
+      p <- ggplot(data = df(), aes_string(x = input$parameter, y = "pressure")) + geom_point()
+      p <- p + scale_y_reverse()
+      ggplotly(p) %>% layout(dragmode = "lasso")
+      
+  })
+  
 }
 
 # Run the application 
--- basic_shiny_app.R
+++ basic_shiny_app.R
@@ -11,115 +11,165 @@
 # title: "Visual Quality Control Tool for Inspecting CTD Profile Data"
 # author: "Jeff Jackson"
 # date: "17/09/2019"
-# last updated: "18/09/2019"
+# last updated: "02/10/2019"
 # output: html_document
 # ---
 
 library(shiny)
+library(shinyjs) # shinyjs is handy
 library(plotly)
 library(oce)
 
 ui <- fluidPage(
-  titlePanel("Visual QC Editor"),
-  mainPanel(
-    uiOutput("plot")
-  ),
-  sidebarPanel(
-    
-    # Input: Select a file ----
-    fileInput(inputId = "file1", 
-              label = "Choose ODF File",
-              multiple = FALSE,
-              accept = c("text/odf", ".odf")
-             ),
 
-    # Text box containing file path and name.
-    textInput(inputId = "filepath", label = "Path and Filename:", value = ""),
-    
-    selectInput(inputId = "parameter", label = strong("Select X Parameter to plot against Pressure"), choices = NULL)
+  tags$head(
+    tags$style(HTML('* {font-family: BentonSans Book; font-size: 16px}'))
+  ),
+  
+  # Style horizontal rules (hr).
+  tags$head(
+    tags$style(HTML('hr {border-top: 1px solid #000000}'))
+  ),
+  
+  # Create the title and style it.
+  h1(id="big-heading", "Visual QC Editor"),
+  tags$head(tags$style(
+    HTML('#big-heading {
+      color: red; 
+      font-size: 40px;
+      font-style: bold;
+    }')
+  )),
+  
+  sidebarLayout
+  (
+    sidebarPanel
+    (
+      
+      # Text box containing file path and name.
+      div
+      ( style = "font-size:15px;", 
+        textInput(inputId = "filepath", 
+                  label = div(style = "font-size:20px", "Selected ODF File:"), 
+                  value = ""
+                 )
+      ),
+      
+      # Insert Horizontal Rule (line) ----
+      hr(),
+      
+      selectInput(inputId = "parameter", label = strong("Select X Parameter to plot against Pressure"), choices = NULL),
+
+      actionButton("loadButton","Load ODF File")
+      
+    ),
+
+    mainPanel
+    (
+      textOutput("filep"),
+      textOutput("vars")
+    )
   )
 )
 
 server <- (function(input, output, session) {
   
-  observe({
-    # if(!exists(input$parameter)) return()
-    
-    vars <- names(plot.ctd)
-    
-    # Display the current ODF file with path.
-    updateTextInput(session, "filepath", value = input$file1)
+  re1 <- eventReactive(
+    input$loadButton, 
+    {
+      fileWithPath <- file.choose()
+    }
+  )
 
-    # Update the parameter list based on the current ODF file.
-    updateSelectInput(session, "parameter", choices = vars)
+  output$filep <- renderText({
+    re1()
   })
   
-  output$plot <- renderUI({
-    plotOutput("p")
-  })
+  re2 <- eventReactive(
+    input$loadButton, 
+    {
+      df <- dfObject()
+    }
+  )
+  
+  # output$plot <- renderUI({
+  #   plotOutput("p")
+  # })
+  
+  # Update the parameter list based on the current ODF file.
+  # updateSelectInput(session, "parameter", choices = "")
+  
+  # Get file with path.
+  ctdObject <- function()
+  {
+    # thePath <- 'E:/Data/AZMP/2019/COR2019001/DATASHOP_PROCESSING/Step_3_Add_QF_Fields/'
+    # mypath <- choose.dir(default = thePath, caption = "Select folder")
+    # as.character(file.choose())
+    # fileWithPath <- paste(mypath,'/', myFile, sep = "")
+    odfFile <- basename(fileWithPath)
+    read.ctd(fileWithPath)
+  }
   
   # Get the data object
-  get_data <- reactive({
-    
-    # if(!exists(input$dataset)) return() # if no upload
-    
-    # if(check(input$dataset)) return()
-    
-    # Initialize the flag scheme.
-    # print(paste("Initializing flag scheme for file: ", input$file1))
+  dfObject <- function()
+  {
+    ctd <- ctdObject()
 
-    ctd <- upload_data()
-    
+    # Display the current ODF file with path.
+    updateTextInput(session, "filepath", value = odfFile)
+
     # Initialize the flag scheme.
-    ctd <- initializeFlagScheme(ctd, "DFO")
+    # ctd <- oce::initializeFlagScheme(ctd, "DFO")
 
     df_data <- data.frame(ctd[['data']])
-    df_flags <- data.frame(ctd[['metadata']]$flags)
-    nms <- row.names(df_data)
-    
-    if(check(df_data)) return()
-    
+
+    # df_flags <- data.frame(ctd[['metadata']]$flags)
+    # nms <- row.names(df_data)
+    # if(check(df_data)) return()
     df_data
-    
-  })
+  }
 
+  # output$vars <- function()
+  # {
+  #   df <- dfObject()
+  #   names(df)
+  # }
+
+  # vars <- function()
+  # {
+  #   myObj <- getData()
+  #   vars <- names(myObj)
+  #   print("Nice Melons")
+  # }
+  
+  # output$varlist <- renderPrint({
+  #   p <- vars()
+  #   print(p[1])
+  # })
+  
+  # Output filename as text to MainPanel.
+  # output$filename <- renderText({
+  #   as.character(input$file1[1])
+  # })
+  
   #plotting function using ggplot2
-  output$p <- renderPlot({
-
-    plot.ctd <- get_data()
-
-    # Conditions for plotting
-    if(is.null(plot.ctd)) return()
-
-    # Make sure variable has been loaded
-    if(plot.ctd$variable == "") return()
-
-    output$plot <- renderPlotly({
-      p <- ggplot(data = plot.ctd$df_data, aes(x = plot.ctd$variable, y = pressure, key = nms)) + geom_point()
-      p <- p + scale_y_reverse()
-      ggplotly(p) %>% layout(dragmode = "lasso")
-    })
-
-  })
-  
-  
-  # set uploaded file
-  upload_data <- reactive({
-    
-    # Load the CTD data
-    # path <- 'E:/Data/AZMP/2019/COR2019001/DATASHOP_PROCESSING/Step_3_Add_QF_Fields/'
-    # filesWithPath <- list.files(path = path, pattern = 'CTD_COR2019001_01\\w+_\\w+_DN\\.ODF', full.names = TRUE)
-    # files <- list.files(path = path, pattern = 'CTD_COR2019001_01\\w+_\\w+_DN\\.ODF', full.names = FALSE)
-    # ctds <- lapply(filesWithPath, read.ctd)
-
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    #could also store in a reactiveValues
-    read.ctd(inFile$datapath)
-  })
+  # output$p <- renderPlot({
+  # 
+  #   plot.ctd <- get_data()
+  # 
+  #   # Conditions for plotting
+  #   if(is.null(plot.ctd)) return()
+  # 
+  #   # Make sure variable has been loaded
+  #   if(plot.ctd$variable == "") return()
+  # 
+  #   output$plot <- renderPlotly({
+  #     p <- ggplot(data = plot.ctd$df_data, aes(x = plot.ctd$variable, y = pressure, key = nms)) + geom_point()
+  #     p <- p + scale_y_reverse()
+  #     ggplotly(p) %>% layout(dragmode = "lasso")
+  #   })
+  # 
+  # })
   
 })
 
