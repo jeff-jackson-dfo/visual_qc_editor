209d25771fa0f3656209926ea51e3af23766d158
Jeff Jackson <Jeffrey.Jackson@dfo-mpo.gc.ca>
2019-10-08 13:13:01 -0300

    Another iteration

--- VisualEditor/app.R
+++ VisualEditor/app.R
@@ -1,4 +1,3 @@
-
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -9,154 +8,179 @@
 #
 
 library(shiny)
+library(shinyjs)
 library(shinyWidgets)
 library(oce)
 library(plotly)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  tags$head(
-    tags$style(HTML('* {font-family: BentonSans Book; font-size: 16px}'))
-  ),
-  
-  # Style horizontal rules (hr).
-  tags$head(
-    tags$style(HTML('hr {border-top: 1px solid #000000}'))
-  ),
-  
-  # Create the title and style it.
-  h1(id="big-heading", "Visual QC Editor"),
-  tags$head(tags$style(
-    HTML('#big-heading {
-      color: red; 
+    
+    # Use a CSS theme. The file is in the "www" subfolder.
+    theme = "bootstrap.pulse.css",
+    
+    # Call to tell the UI that this app is using the shinyjs package.
+    useShinyjs(),
+    
+    # window_height <- shinyjs::js('window.innerHeight'),
+    # window_width <- shinyjs::js('window.innerWidth'),
+
+    tags$head(tags$style(
+        HTML('* {font-family: BentonSans Book; font-size: 16px}')
+    )),
+    
+    # Style horizontal rules (hr).
+    tags$head(tags$style(HTML(
+        'hr {border-top: 1px solid #000000}'
+    ))),
+    
+    # Create the title and style it.
+    h1(id = "big-heading", "Visual QC Editor"),
+    tags$head(tags$style(
+        HTML('#big-heading {
+      color: red;
       font-size: 40px;
       font-style: bold;
     }')
-  )),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      
-      br(),
-      
-      # Press button to load an ODF file.
-      p(style = "text-align: center; border-color: #f00", 
-        actionButton("loadButton",tags$b("Load ODF File"))),
-      tags$style("#loadButton{color: blue; font-size: 20px; background-color: yellow; border-color: black; border-width: 5px}"),
-      
-      br(),
-      
-      # Text box containing file path and name.
-      div
-      ( style = "font-size:20px;", 
-        textInput(inputId = "filepath", 
-                  label = div(style = "font-size:20px", "Selected ODF File:"), 
-                  value = ""
-        )
-      ),
-      
-      # Insert Horizontal Rule (line) ----
-      # hr(),
-      
-      selectInput(inputId = "parameter", label = strong("Select X Parameter:"), choices = NULL)
-      
-    ),
+    )),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      
-      # textOutput("filep"),
-      # textOutput("params"),
-      # textOutput("cn"),
-      # textOutput("param"),
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            br(),
+            
+            # Press button to load an ODF file.
+            p(style = "text-align: center; border-color: #f00",
+              actionButton("loadButton", tags$b("Load ODF File"))),
+            tags$style(
+                "#loadButton{color: blue; font-size: 20px; background-color: yellow; border-color: black; border-width: 5px}"
+            ),
+            
+            br(),
+            
+            # Text box containing file path and name.
+            div
+            (style = "font-size:20px;",
+                textInput(
+                    inputId = "filepath",
+                    label = div(style = "font-size:20px", "Selected ODF File:"),
+                    value = ""
+                )),
+            
+            # Insert Horizontal Rule (line) ----
+            # hr(),
+            
+            selectInput(
+                inputId = "parameter",
+                label = strong("Select X Parameter:"),
+                choices = NULL
+            )
+            
+        ),
         
-      plotlyOutput("profilePlot")
-      
+        # Show a plot of the generated distribution
+        mainPanel(# textOutput("filep"),
+            # textOutput("params"),
+            # textOutput("cn"),
+            # textOutput("param"),
+            
+            plotlyOutput(
+                "profilePlot", width = "100%", height = "100%"
+            )
+        )
     )
-  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  
-  re1 <- eventReactive(
-    input$loadButton, 
-    {
-      fileWithPath <- file.choose()
-    }
-  )
-  
-  output$filep <- renderText({
-    odfFile <- basename(re1())
-  })
-  
-  observe({
+    re1 <- eventReactive(input$loadButton,
+                         {
+                             fileWithPath <- file.choose()
+                         })
     
-    # Display the current ODF file with path.
-    odfFile <- basename(re1())
+    output$filep <- renderText({
+        odfFile <- basename(re1())
+    })
     
-    # Display the current ODF file without path in the textInput box.
-    updateTextInput(session, "filepath", value = as.character(odfFile))
+    observe({
+        # Display the current ODF file with path.
+        odfFile <- basename(re1())
+        
+        # Display the current ODF file without path in the textInput box.
+        updateTextInput(session, "filepath", value = as.character(odfFile))
+        
+    })
     
-  })
-  
-  odfData <- reactiveValues()
-  
-  observe({
-    # Read in the CTD data.
-    ctd <- read.ctd(re1())
-    odfData$dfCTD <- as.data.frame(ctd[['data']])
-    odfData$lMeta <- as.list(ctd[['metadata']])
-    # nms <- as.numeric(row.names(odfData$dfCTD))
-  })
-  
-  output$cn <- renderText({
-    print(odfData$lMeta$cruiseNumber)
-  })
-  
-  vars <- reactive({
-    vars <- names(odfData$dfCTD)
-  })
-  
-  output$nms <- reactive({
-    print(odfData$nms)
-  })
-  
-  observe({
-    updateSelectInput(session, "parameter", choices = vars(), selected = "temperature")
-  })
-  
-  var <- reactive({
-      input$parameter
-  })
-  
-  output$param <- renderText({
-    var()   
-  })
-  
-  df <- reactive({
-      odfData$dfCTD
-  })
-  
-  output$profilePlot <- renderPlotly({
-      
-      # Make sure that the selectInput drop down list box contains something before continuing.
-      req(input$parameter)
-      
-      # If there is no CTD loaded yet then do nothing.
-      if (is.null(df())) {return()}
-      
-      # Produce the profile plot.
-      p = ggplot()
-      p <- ggplot(data = df(), aes_string(x = input$parameter, y = "pressure")) + geom_point()
-      p <- p + scale_y_reverse()
-      ggplotly(p) %>% layout(dragmode = "lasso")
-      
-  })
-  
+    odfData <- reactiveValues()
+    
+    observe({
+        # Read in the CTD data.
+        ctd <- read.ctd(re1())
+        
+        # Initialize the flag scheme.
+        ctd <- oce::initializeFlagScheme(ctd, "DFO")
+        
+        # Coerce the CTD object's data list into an external data frame.
+        odfData$dfCTD <- as.data.frame(ctd[['data']])
+        
+        # Coerce the CTD object's metadata list into an external list.
+        odfData$lMeta <- as.list(ctd[['metadata']])
+        
+        # nms <- as.numeric(row.names(odfData$dfCTD))
+    })
+    
+    output$cn <- renderText({
+        print(odfData$lMeta$cruiseNumber)
+    })
+    
+    vars <- reactive({
+        
+        vars <- names(odfData$dfCTD)
+    })
+    
+    output$nms <- reactive({
+        print(odfData$nms)
+    })
+    
+    observe({
+        updateSelectInput(session,
+                          "parameter",
+                          choices = vars(),
+                          selected = "temperature")
+    })
+    
+    var <- reactive({
+        input$parameter
+    })
+    
+    output$param <- renderText({
+        var()
+    })
+    
+    df <- reactive({
+        odfData$dfCTD
+    })
+    
+    output$profilePlot <- renderPlotly({
+        # Make sure that the selectInput drop down list box contains something before continuing.
+        req(input$parameter)
+        
+        # If there is no CTD loaded yet then do nothing.
+        if (is.null(df())) {
+            return()
+        }
+        
+        # Produce the profile plot.
+        p = ggplot()
+        p <-
+            ggplot(data = df(), aes_string(x = input$parameter, y = "pressure")) + geom_point()
+        p <- p + theme_bw()
+        p <- p + scale_y_reverse()
+        ggplotly(p) %>% layout(dragmode = "lasso")
+        
+    })
+    
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
